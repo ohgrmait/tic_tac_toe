5060b9d143ec8f0d2f1925ff3ec229a1985142dd
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-16 16:52:51 +0530

    Update the board as the players play the game

--- lib/game.rb
+++ lib/game.rb
@@ -15,4 +15,15 @@ class Game
       puts '----|-----|----'
     end
   end
+
+  def update_board(choice, grid_no)
+    grid_count = 0
+    @board.each_with_index do |row, row_idx|
+      row.each_with_index do |column, column_idx|
+        grid_count += 1
+        @board[row_idx][column_idx] = choice if grid_count == grid_no &&
+                                                (column.instance_of? Integer)
+      end
+    end
+  end
 end
