79c24c4a96156746e2f5e029eb3e8acc580a5a42
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-16 16:53:42 +0530

    Play the game to check if the board updates

--- main.rb
+++ main.rb
@@ -1,4 +1,19 @@
 require_relative 'lib/game'
+require_relative 'lib/player'
 
 game = Game.new
+player = Player.new
+
+game.display_board
+game.update_board(player.turn, 5)
+puts ''
+game.display_board
+game.update_board(player.turn, 3)
+puts ''
+game.display_board
+game.update_board(player.turn, 8)
+puts ''
+game.display_board
+game.update_board(player.turn, 1)
+puts ''
 game.display_board
