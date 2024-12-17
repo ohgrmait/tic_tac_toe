0573f46849e58299a7f3b773763159a2c4ac804d
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-17 09:07:11 +0530

    Add the logic to test if the game is won or drawn
    Test if the game is over - for the game to be over, it has to be either
    won or drawn.
    
    To check for win conditions - one check for horizontal match, one check
    for vertical match, and two checks for both diagonal matches were
    placed.
    
    To check for draw condition - one check to test if the multidimensional
    array does not contain any element that is an instance of an integer
    needs to be placed.

--- lib/game.rb
+++ lib/game.rb
@@ -26,4 +26,49 @@ class Game
       end
     end
   end
+
+  def drawn?
+    @board.flatten.count { |elem| elem.instance_of? Integer }.zero?
+  end
+
+  def second_diagonal_match?
+    second_diagonal = (0..@board.size - 1).collect do |index|
+      @board[index][-index - 1]
+    end
+    second_diagonal.all? { |elem| elem == 'X' } ||
+      second_diagonal.all? { |elem| elem == 'O' }
+  end
+
+  def first_diagonal_match?
+    first_diagonal = (0..@board.size - 1).collect do |index|
+      @board[index][index]
+    end
+    first_diagonal.all? { |elem| elem == 'X' } ||
+      first_diagonal.all? { |elem| elem == 'O' }
+  end
+
+  def diagonal_match?
+    true if first_diagonal_match? || second_diagonal_match?
+  end
+
+  def vertical_match?
+    @board.transpose.any? do |row|
+      row.all? { |elem| elem == 'X' } || row.all? { |elem| elem == 'O' }
+    end
+  end
+
+  def horizontal_match?
+    @board.any? do |row|
+      row.all? { |elem| elem == 'X' } || row.all? { |elem| elem == 'O' }
+    end
+  end
+
+  def won?
+    true if horizontal_match? || vertical_match? ||
+            diagonal_match?
+  end
+
+  def over?
+    true if won? || drawn?
+  end
 end
