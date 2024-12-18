c744ac9357f38c8c6d0b9da6e4c6bef0066a9157
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-18 14:05:34 +0530

    Refactor main.rb for TicTacToe class to start game

--- main.rb
+++ main.rb
@@ -1,38 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'lib/tic_tac_toe'
+
 require 'colorize'
 
-require_relative 'lib/game'
-require_relative 'lib/player'
-
-game = Game.new
-player = Player.new
-
-puts '...A game of Tic Tac Toe between two players...'
-puts ''
-puts '...Player X (PX) and O (PO) can alternate and..'
-puts '...mark the grids from 1 to 9 to play the game.'
-puts ''
-game.display_board
-puts ''
-
-until game.over?
-  turn = player.turn
-
-  print "...[P#{turn}] Grid? "
-  grid_no = gets.chomp.to_i
-
-  game.update_board(turn, grid_no)
-  game.display_board
-end
-
-puts ''
-
-if game.won?
-  if turn == 'X'
-    puts "...Congratulations! Player #{turn} has won the game...".colorize(color: :light_red, mode: :bold)
-  elsif turn == 'O'
-    puts "...Congratulations! Player #{turn} has won the game...".colorize(color: :light_yellow, mode: :bold)
-  end
-elsif game.drawn?
-  puts '...What a bummer! Both players have drawn the game...'.colorize(color: :light_green, mode: :bold)
-  puts '...Wow! Get back to grade school you two! Pathetic...'.colorize(color: :light_green, mode: :bold)
-end
+tic_tac_toe = TicTacToe.new
+tic_tac_toe.start_game
