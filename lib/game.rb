037922094434a1e2e7cf8da4d6f0e9dbf279acf8
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-18 13:59:28 +0530

    Refactor the Game class to fit a line length of 80

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,6 @@
-class Game
+# frozen_string_literal: true
+
+class Game # rubocop:disable Style/Documentation
   def initialize
     @board = [
       [1, 2, 3],
@@ -16,58 +18,29 @@ class Game
     end
   end
 
-  def update_board(choice, grid_no)
+  def update_board(choice, grid_no) # rubocop:disable Metrics/MethodLength
     grid_count = 0
     @board.each_with_index do |row, row_idx|
       row.each_with_index do |column, column_idx|
         grid_count += 1
-        if grid_count == grid_no && (column.instance_of? Integer)
+        next unless grid_count == grid_no && (column.instance_of? Integer)
+
+        @board[row_idx][column_idx] =
           if choice == 'X'
-            @board[row_idx][column_idx] = choice.colorize(color: :light_red, mode: :bold)
-          elsif choice == 'O'
-            @board[row_idx][column_idx] = choice.colorize(color: :light_yellow, mode: :bold)
+            choice.colorize(color: :light_red, mode: :bold)
+          else
+            choice.colorize(color: :light_yellow, mode: :bold)
           end
-        end
       end
     end
   end
 
+  def over?
+    true if drawn? || won?
+  end
+
   def drawn?
     @board.flatten.count { |elem| elem.instance_of? Integer }.zero?
-  end
-
-  def second_diagonal_match?
-    second_diagonal = (0..@board.size - 1).collect do |index|
-      @board[index][-index - 1]
-    end
-    second_diagonal.all? { |elem| elem == 'X'.colorize(color: :light_red, mode: :bold) } ||
-      second_diagonal.all? { |elem| elem == 'O'.colorize(color: :light_yellow, mode: :bold) }
-  end
-
-  def first_diagonal_match?
-    first_diagonal = (0..@board.size - 1).collect do |index|
-      @board[index][index]
-    end
-    first_diagonal.all? { |elem| elem == 'X'.colorize(color: :light_red, mode: :bold) } ||
-      first_diagonal.all? { |elem| elem == 'O'.colorize(color: :light_yellow, mode: :bold) }
-  end
-
-  def diagonal_match?
-    true if first_diagonal_match? || second_diagonal_match?
-  end
-
-  def vertical_match?
-    @board.transpose.any? do |row|
-      row.all? { |elem| elem == 'X'.colorize(color: :light_red, mode: :bold) } ||
-        row.all? { |elem| elem == 'O'.colorize(color: :light_yellow, mode: :bold) }
-    end
-  end
-
-  def horizontal_match?
-    @board.any? do |row|
-      row.all? { |elem| elem == 'X'.colorize(color: :light_red, mode: :bold) } ||
-        row.all? { |elem| elem == 'O'.colorize(color: :light_yellow, mode: :bold) }
-    end
   end
 
   def won?
@@ -75,7 +48,61 @@ class Game
             diagonal_match?
   end
 
-  def over?
-    true if won? || drawn?
+  def horizontal_match?
+    @board.any? do |row|
+      row.all? do |elem|
+        elem == 'X'.colorize(color: :light_red,
+                             mode: :bold)
+      end ||
+        row.all? do |elem|
+          elem == 'O'.colorize(color: :light_yellow,
+                               mode: :bold)
+        end
+    end
+  end
+
+  def vertical_match?
+    @board.transpose.any? do |row|
+      row.all? do |elem|
+        elem == 'X'.colorize(color: :light_red,
+                             mode: :bold)
+      end ||
+        row.all? do |elem|
+          elem == 'O'.colorize(color: :light_yellow,
+                               mode: :bold)
+        end
+    end
+  end
+
+  def diagonal_match?
+    true if first_diagonal_match? || second_diagonal_match?
+  end
+
+  def first_diagonal_match? # rubocop:disable Metrics/MethodLength
+    first_diagonal = (0..@board.size - 1).collect do |index|
+      @board[index][index]
+    end
+    first_diagonal.all? do |elem|
+      elem == 'X'.colorize(color: :light_red,
+                           mode: :bold)
+    end ||
+      first_diagonal.all? do |elem|
+        elem == 'O'.colorize(color: :light_yellow,
+                             mode: :bold)
+      end
+  end
+
+  def second_diagonal_match? # rubocop:disable Metrics/MethodLength
+    second_diagonal = (0..@board.size - 1).collect do |index|
+      @board[index][-index - 1]
+    end
+    second_diagonal.all? do |elem|
+      elem == 'X'.colorize(color: :light_red,
+                           mode: :bold)
+    end ||
+      second_diagonal.all? do |elem|
+        elem == 'O'.colorize(color: :light_yellow,
+                             mode: :bold)
+      end
   end
 end
