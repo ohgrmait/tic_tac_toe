039ea3ecf45f69906aa6a90b736d7a00fc6a1297
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-19 11:28:54 +0530

    Clean the user input to get a valid move only

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,15 +25,29 @@ class TicTacToe # rubocop:disable Style/Documentation
     puts ''
   end
 
+  def get_input(turn) # rubocop:disable Metrics/MethodLength
+    print "...[P#{turn}] Grid? "
+    grid_no = gets.chomp
+    raise StandardError if (grid_no.chars.size > 1) ||
+                           (grid_no.to_i < 1 || grid_no.to_i > 9)
+  rescue StandardError
+    puts ''
+    puts '!!![You can only enter values between 1 and 9]'
+      .colorize(color: :light_red, mode: :bold, background: :light_yellow)
+    puts ''
+    retry
+  else
+    grid_no.to_i
+  end
+
   def play_game
     until @game.over?
       turn = @player.turn
-      print "...[P#{turn}] Grid? "
-      grid_no = gets.chomp.to_i
+      grid_no = get_input(turn)
       @game.update_board(turn, grid_no)
       @game.display_board
+      puts ''
     end
-    puts ''
   end
 
   def end_game # rubocop:disable Metrics/MethodLength
